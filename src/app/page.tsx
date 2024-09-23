db8ff686d5e0ca375ef01a5c737683df512df314
deeMswazi <denzelmwangi94@gmail.com>
2024-09-23 13:02:02 +0300

    feature/bug fix-display logo on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { Metadata } from "next";
-
 import { SliceZone } from "@prismicio/react";
 import * as prismic from "@prismicio/client";
-
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 
@@ -14,22 +12,40 @@ import { components } from "@/slices";
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const home = await client.getByUID("page", "home");
+  
+  try {
+    const home = await client.getByUID("page", "home");
 
-  return {
-    title: prismic.asText(home.data.title),
-    description: home.data.meta_description,
-    openGraph: {
-      title: home.data.meta_title ?? undefined,
-      images: [{ url: home.data.meta_image.url ?? "" }],
-    },
-  };
+    return {
+      title: prismic.asText(home.data.title),
+      description: home.data.meta_description || "",
+      openGraph: {
+        title: home.data.meta_title || undefined,
+        images: [{ url: home.data.meta_image?.url || "" }],
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching document:", error);
+    return {
+      title: "Default Title",
+      description: "Default description",
+      openGraph: {
+        title: "Default Title",
+        images: [],
+      },
+    };
+  }
 }
 
 export default async function Index() {
   // The client queries content from the Prismic API
   const client = createClient();
-  const home = await client.getByUID("page", "home");
-
-  return <SliceZone slices={home.data.slices} components={components} />;
+  
+  try {
+    const home = await client.getByUID("page", "home");
+    return <SliceZone slices={home.data.slices} components={components} />;
+  } catch (error) {
+    console.error("Error fetching document:", error);
+    return <div>Error loading content</div>; // You can customize this as needed
+  }
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { FizziLogo } from '@/components/FizziLogo'
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header className="flex justify-center py-4">
-
+    <header className="flex justify-center py-4 -mb-28">
+      <FizziLogo />
     </header>
   )
 }
